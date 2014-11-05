71815e8b4355898fb73801f715351d6a79920eda
crisro96 <crisro996@gmail.com>
2014-11-05 23:45:40 +0200

    v. finala

--- Main.java
+++ Main.java
@@ -59,10 +59,6 @@ public class Main
 
         Scanner input = new Scanner(System.in);
 
-        for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++)
-                myArray[i][j] = 0;
-
         System.out.println("Dimensiunea matricii(n):");
         size_Array = input.nextInt();
 
